052d8d8554dd659f9e2adca51e293f9d5b685e4f
T-Rizzle <trentontome@icloud.com>
2026-01-03 14:18:06 -0500

    Improved Loot Transfer logic
    Bots assigned to transfering loot will now properly cycle between all entrances to the facility they can reach. They will keep cycling until their inventory is full.

--- AI/AIStates/TransferLootState.cs
+++ AI/AIStates/TransferLootState.cs
@@ -122,17 +122,33 @@ namespace LethalBots.AI.AIStates
                         // No loot at our target entrance, lets check the other entrances
                         // NOTE: Bots will cycle through all entrances before returning to ship.
                         waitTimer = 0f;
-                        checkedEntrances.Add(targetEntrance); // Mark this entrance as checked
+
+                        EntranceTeleport? previousEntrance = targetEntrance;
+                        checkedEntrances.Add(previousEntrance); // Mark this entrance as checked
                         targetEntrance = FindClosestEntrance(entrancesToAvoid: checkedEntrances); // Find another entrance to check out
-                        if (targetEntrance != null && !checkedEntrances.Contains(targetEntrance))
+                        if (targetEntrance != null 
+                            && !checkedEntrances.Contains(targetEntrance) 
+                            && previousEntrance != targetEntrance)
                         {
                             // Found another entrance to check out, head over there
                             return;
                         }
 
-                        // No other entrance found, return to ship
-                        ai.State = new ReturnToShipState(this);
-                        return;
+                        // No other entrance found
+                        if (ai.HasSomethingInInventory())
+                        {
+                            // We have something in our inventory, return to ship to drop it off
+                            ai.State = new ReturnToShipState(this);
+                            return;
+                        }
+                        else
+                        {
+                            // Nothing in inventory, reset target entrance and checked list to start over
+                            targetEntrance = null;
+                            checkedEntrances.Clear(); // Reset checked entrances
+                            checkedEntrances.Add(previousEntrance); // Mark previous entrance as checked to avoid immediately going back
+                            return;
+                        }
                     }
                 }
                 else
